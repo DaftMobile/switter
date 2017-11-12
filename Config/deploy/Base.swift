106489891ae4a29983e97f74a8bae0882b43564f
Michal Dabrowski <michal.dabrowski@daftcode.pl>
2017-11-12 14:46:13 +0100

    Change repo url

--- Config/deploy/Base.swift
+++ Config/deploy/Base.swift
@@ -5,7 +5,7 @@ class Base: Environment {
 	func configure() {
 		Config.projectName = "Switter"
 		Config.executableName = "Run"
-		Config.repoURL = "ssh://git@gitlab.int.daftcode.pl:2200/mdabrowski/switter.git"
+		Config.repoURL = "git@github.com:DaftMobile/switter.git"
 
 		Config.serverFramework = VaporFramework()
 		Config.processController = Supervisord()
