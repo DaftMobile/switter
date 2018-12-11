4911f91bb4bf0d93575fe164ffb23c3ef7730e8e
Michal Dabrowski <michal.dabrowski@daftcode.pl>
2018-12-12 00:27:00 +0100

    Add remove old releases tast to flock

--- Config/deploy/OrkaTask.swift
+++ Config/deploy/OrkaTask.swift
@@ -2,7 +2,8 @@ import Flock
 
 public extension TaskSource {
    static let orka = TaskSource(tasks: [
-       OrkaTask()
+       OrkaTask(),
+       RemoveOldReleasesTask()
    ])
 }
 
@@ -17,6 +18,30 @@ class OrkaTask: Task {
    func run(on server: Server) throws {
       try invoke("vapor:stop", on: server)
       try server.execute(command)
-      try invoke("vapor:start", on: server)
+   }
+}
+
+class RemoveOldReleasesTask: Task {
+   let name = "remove_old"
+   let namespace = "deploy"
+
+   func find(currentPath: String) -> String {
+      return "find \(Paths.releasesDirectory) -maxdepth 1 -type d ! -path \(Paths.releasesDirectory) ! -path \(currentPath)"
+   }
+
+   func remove(directory: String) -> String {
+      return "rm -rf \(directory)"
+   }
+
+   func run(on server: Server) throws {
+      let currentPath = try server.capture("readlink \(Paths.currentDirectory)").trimmingCharacters(in: .whitespacesAndNewlines)
+      let pathsToDelete = try server
+         .capture(find(currentPath: currentPath))
+         .components(separatedBy: .newlines)
+         .filter { !$0.isEmpty }
+         .filter { $0.hasPrefix(Paths.releasesDirectory) }
+      for path in pathsToDelete {
+         try server.execute(remove(directory: path))
+      }
    }
 }
